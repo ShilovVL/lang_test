e63983f4e6e300982f4864913a8eee78a922b643
ShilovVL <v.shilov.tomsk@gmail.com>
2021-03-02 10:45:36 +0700

    add sleep(30)

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_basket(browser):
     browser.get(link)
+    time.sleep(30)
     b_link = browser.find_element_by_css_selector(".btn-group > a:nth-child(1)")
     assert b_link is not None, "Нет корзины!"
