c0703ed034abda71cd44c1700867c5dd368b24b6
ShilovVL <v.shilov.tomsk@gmail.com>
2021-03-02 10:04:39 +0700

    solutions for the chromedriver

---
+++ test_items.py
@@ -0,0 +1,7 @@
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_basket(browser):
+    browser.get(link)
+    b_link = browser.find_element_by_css_selector(".btn-group > a:nth-child(1)")
+    assert b_link is not None, "Нет корзины!"
